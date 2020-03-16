1cfe48627517615985dabcd94feb890571604ff8
yalab <rudeboyjet@gmail.com>
2020-03-16 19:52:48 +0900

    It can touch but cannot internet access.

--- app/src/main/java/com/example/grand/Widget.kt
+++ app/src/main/java/com/example/grand/Widget.kt
@@ -4,11 +4,20 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import android.content.ComponentName
+import android.app.PendingIntent
+import android.R.attr.action
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import android.content.Intent
+import android.widget.Toast
+
 
 /**
  * Implementation of App Widget functionality.
  */
 class Widget : AppWidgetProvider() {
+    private val ACTION = "open"
+    var lock = false
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -18,6 +27,13 @@ class Widget : AppWidgetProvider() {
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
+        val remoteViews = RemoteViews(context.getPackageName(), R.layout.widget)
+        val watchWidget = ComponentName(context, Widget::class.java)
+        val intent = Intent(context, javaClass)
+        intent.setAction(ACTION)
+        val peindingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        remoteViews.setOnClickPendingIntent(R.id.open_button, peindingIntent)
+        appWidgetManager.updateAppWidget(watchWidget, remoteViews);
     }
 
     override fun onEnabled(context: Context) {
@@ -26,6 +42,18 @@ class Widget : AppWidgetProvider() {
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        if (!lock && ACTION.equals(intent.getAction())) {
+            lock = true
+            Toast.makeText(context, ACTION, 1).show()
+            Thread(Runnable {
+                Webhook().call()
+            }).start()
+        }
     }
 }
 
@@ -37,7 +65,6 @@ internal fun updateAppWidget(
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
